d8b16300a2287c83bd7bf058ccbf76c6d320fd27
Victor Hernandez <victormanu968@gmail.com>
2023-08-28 14:47:20 -0500

    Listas Factoriales

--- app/src/main/scala/introduccion/ListaFactoriales.scala
+++ app/src/main/scala/introduccion/ListaFactoriales.scala
@@ -5,7 +5,10 @@ class ListaFactoriales {
     def objListaFactoriales(n : Int): List[BigInt] = {
         var listaFactoriales : List[BigInt] = List()
         val objFactorial: Factorial = new Factorial()
-        //Complete el código
-        throw new UnsupportedOperationException("No implementado aún")
+        for (i <- 0 to n){
+            var factorial: BigInt = objFactorial.calculateFactorial(i)
+            listaFactoriales = listaFactoriales :+ factorial
+        }
+        return listaFactoriales
     }
 }
